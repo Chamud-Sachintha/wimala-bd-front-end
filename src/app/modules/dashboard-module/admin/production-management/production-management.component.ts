08b62007bcf1f2e6952559177bc0ab0ff942af77
Chamud Sachintha <sheraanmario777@gmail.com>
2022-11-10 19:22:16 +0530

    line management ui create

--- src/app/modules/dashboard-module/admin/production-management/production-management.component.ts
+++ src/app/modules/dashboard-module/admin/production-management/production-management.component.ts
@@ -1,4 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
+import { Component, OnInit, PipeTransform } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { map, Observable, startWith } from 'rxjs';
+
+interface Country {
+	id?: number;
+	name: string;
+	flag: string;
+	area: number;
+	population: number;
+}
+
+const COUNTRIES: Country[] = [
+	{
+		name: 'Russia',
+		flag: 'f/f3/Flag_of_Russia.svg',
+		area: 17075200,
+		population: 146989754,
+	},
+	{
+		name: 'France',
+		flag: 'c/c3/Flag_of_France.svg',
+		area: 640679,
+		population: 64979548,
+	},
+];
+
+function search(text: string, pipe: PipeTransform): Country[] {
+	return COUNTRIES.filter((country) => {
+		const term = text.toLowerCase();
+		return (
+			country.name.toLowerCase().includes(term) ||
+			pipe.transform(country.area).includes(term) ||
+			pipe.transform(country.population).includes(term)
+		);
+	});
+}
 
 @Component({
   selector: 'app-production-management',
@@ -7,7 +44,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductionManagementComponent implements OnInit {
 
-  constructor() { }
+  countries$!: Observable<Country[]>;
+  filter = new FormControl('');
+  active = 1;
+  constructor(pipe: DecimalPipe) {
+    this.countries$ = this.filter.valueChanges.pipe(
+			startWith(''),
+			map((text) => search(text, pipe)),
+		);
+  }
 
   ngOnInit(): void {
   }
